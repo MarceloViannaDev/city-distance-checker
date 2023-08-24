933344f814e3cf4d354b8be7ab5d7330a707ed8f
Marcelo Vianna Pacheco <marceloviannadev@gmail.com>
2023-08-24 01:45:01 -0300

    :tada structure and mobile design

---
+++ src/components/HomePage.tsx
@@ -0,0 +1,11 @@
+import { Form } from './Form';
+
+export const HomePage = () => {
+  return (
+    <div className="container mx-auto py-[25vh] w-[90%] max-w-[734px] h-screen">
+      <div className="text-sm font-medium bg-white rounded-2xl">
+        <Form />
+      </div>
+    </div>
+  );
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -9,12 +9,15 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        backgroundMain: "url('/bg__main_app.png')",
+      },
+      colors: {
+        purple: '#9747FF',
+        purpleLight: '#C7D1F4',
+        lime: '#DBF9B3',
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
