6065bb872684a7090dbb6a9e3b01405b042ad080
Marcelo Vianna Pacheco <marceloviannadev@gmail.com>
2023-08-24 22:38:32 -0300

    feat: some adjustments

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,4 +1,4 @@
-import { Circle } from 'lucide-react';
+import { Circle, Dot, MapPin } from 'lucide-react';
 import { AddDestination } from './buttons/AddDestination';
 import { Submit } from './buttons/Submit';
 import { CityDestination } from './inputs/CityDestination';
@@ -10,14 +10,26 @@ export const Form = () => {
   return (
     <form action="" className="flex flex-col gap-8 p-4 lg:px-20 lg:py-16">
       <div className="flex flex-col gap-4 lg:gap-8 lg:flex-row">
+        {/* Wrapper container */}
         <div className="flex w-full">
-          <div className="bg-gray-100 ">
-            <Circle className="w-3" />
+          <div className="flex flex-col items-center justify-center pb-[0.75rem]">
+            <Circle className="w-4" />
+            <Dot size={14} strokeWidth={0.5} />
+            <Dot size={14} strokeWidth={0.5} />
+            <Dot size={14} strokeWidth={0.5} />
+            <Dot size={14} strokeWidth={0.5} />
+            <MapPin className="w-4 text-red-500" />
+            <Circle className="w-4" />
+            <Dot size={14} strokeWidth={0.5} />
+            <Dot size={14} strokeWidth={0.5} />
+            <Dot size={14} strokeWidth={0.5} />
+            <Dot size={14} strokeWidth={0.5} />
           </div>
           {/* PATH ICONS CONTAINER */}
 
-          <div className="flex flex-col w-full gap-4 px-10 lg:gap-6 lg:pl-8">
+          <div className="flex flex-col w-full gap-4 px-8 lg:gap-6 lg:pl-8">
             <CityOrigin />
+            <CityDestination />
             <CityDestination />
             <AddDestination />
           </div>
@@ -28,8 +40,8 @@ export const Form = () => {
           <Date />
           <Passengers />
         </div>
+        {/* DATE & PASSENGERS CONTAINER */}
       </div>
-      {/* DATE & PASSENGERS CONTAINER */}
 
       <div className="lg:w-sm lg:flex lg:justify-center">
         <Submit />
